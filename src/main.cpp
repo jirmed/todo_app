e5853504f07b53e6a9c73b76ccaba010234ed836
Jiri Medved <jmedved@bvv.cz>
2025-06-26 14:37:51 +0200

    Switch `TaskManager` to use `FileRepository` with file-based task persistence in `main.cpp`.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "service/TaskManager.h"
 #include "http/RestServer.h"
 #include "service/InMemoryTaskRepository.h"
+#include "service/FileRepository.h"
 
 int main() {
-    auto repository = std::make_unique<InMemoryTaskRepository>();
+//    auto repository = std::make_unique<InMemoryTaskRepository>();
+    auto repository = std::make_unique<FileRepository>("data/tasks.csv");
     TaskManager manager(std::move(repository));
     // std::unique_ptr<UI> ui = std::make_unique<ConsoleUI>(manager);
     // ui->run();
